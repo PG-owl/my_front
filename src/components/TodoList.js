b251ffa961a962bc127705cb18fc247a35078ba2
T.T <your.email@example.com>
2022-04-12 00:13:36 +0900

    implementation Input function & create function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,77 +6,75 @@ import "./style.scss";
 function TodoList() {
   const [todos, setTodos] = useState([]);
   const [task, setTask] = useState("");
-  const [updateTask, setUpdateTask] = useState("");
 
   // タスク表示機能
   useEffect(() => {
     axios.get('http://localhost:3001/api/v1/posts')
          .then(res => {setTodos(res.data)})
-         .catch(res => console.log(res))
-         .catch(res => console.log(res.data))
-  },[])
+         .catch(error => console.log(error))
+  },[]);
   
   // タスク入力機能
   const handleNewTask = (e) => {
-    console.log("イベント発火")
-    axios.post('http://localhost:3001/api/v1/posts',
-      {task: task}
-    ).then(response => {
-      console.log("registration response", response.data)
-      setTodos((todos) => [...todos, { 
-        task: response.data.task, 
-        isCompleted: response.data.isCompleted
-      }])
-      resetTextField()
-    }).catch(error => {
-      console.log("registration error", error)
-    }).catch(data => {
-      console.log(data)
-    })
-    e.preventDefault()
+    setTask(e.target.value)
   };
 
-  const resetTextField = () => {
-    setTask('')
-  }
-
   // タスク追加機能
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   if (task === "") return;
-  //   setTodos((todos) => [...todos, { task, isCompleted: false }]);
-  //   setTask("");
-  // };
+  const handleSubmit = () => {
+    if (task === "") return;
+    axios.post('http://localhost:3001/api/v1/posts', {
+      task: task
+    })
+    .then(res => {
+      console.log(res.data)
+      setTodos([...todos, { 
+        task: res.data.task
+      }])
+      setTask('');
+    })
+    .catch(error => {console.log(error)
+    })
+    task.preventDefault();
+  };
+
+  // タスク削除機能
+  const handleRemoveTask = (id) => {
+    axios.delete('http://localhost:3001/api/v1/posts/${id}')
+    .then(res => {
+      console.log(res.data)
+      setTodos(todos.filter(todo => todo.id !== id))
+    })
+    .catch(error => {console.log(error)
+    })
+  };
   
   // タスク更新機能
   const handleUpdateTask = id => {
-    let newTodos = todos.map((todo,todoId) => {
+    const newTodos = todos.map((todo,todoId) => {
       if(todoId  === id){
           todo.isCompleted = !todo.isCompleted
       }
       return todo;
+    });
+
+    axios.patch('http://localhost:3001/api/v1/posts/${id}',
+      { }
+    ).then(response => {
+      console.log("registration response", response.data)
+      setTodos({newTodos})
     })
-    setTodos(newTodos);
-  }
-  
-  // タスク削除機能
-  const handleRemoveTask = id => {
-    const newTodos = [...todos]
-    newTodos.splice(id,1)
-    setTodos(newTodos)
-  }
+  };
 
   return (
     <div>
       <h1>ToDo List</h1>
 
-      <form onSubmit={handleNewTask}>
+      <form onSubmit={ handleSubmit }>
         <input
           type="text"
-          name="task"
           value={task}
           placeholder="Add New Task"
-          onChange={(e) => setTask(e.target.value)}
+          onChange={ handleNewTask }
         />
         : 
         <button

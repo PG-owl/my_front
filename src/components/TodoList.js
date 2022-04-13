9247a06dbe57ba289b251346e506d0586079df85
T.T <your.email@example.com>
2022-04-14 00:51:05 +0900

    implementation update function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import "./style.scss";
+import "./TodoList.scss";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
   const [task, setTask] = useState("");
+  const [updateTask, setUpdateTask] = useState("");
 
   // タスク表示機能
   useEffect(() => {
@@ -47,14 +48,29 @@ function TodoList() {
     .catch(data => {console.log(data)
     })
   };
+
+  // const updateTask = (id) => {
+  //   if (updatetask === "") return;
+  //   axios.pathe(`http://localhost:3001/api/v1/posts/${id}`,
+  //   {
+  //     task: updatetask
+  //   }
+  //   ).then(res => {
+  //     setTodos(todos.filter(todo => todo.id !== id))
+  //     console.log(res.data)
+  //   }).catch(data =>  {
+  //     console.log(data)
+  //   })
+  // }
   
   // 達成状況 更新機能
   const handleUpdateTask = (id) => {
     axios.patch(`http://localhost:3001/api/v1/posts/${id}`,
       {
-        isCompleted: !isCompleted
+        task: "Compreted"
       }
     ).then(res => {
+      console.log("update")
       let newTodos = todos.map(todo => {
         if(todo.id === res.data.id){
           return res.data;
@@ -67,40 +83,62 @@ function TodoList() {
   };
 
   return (
-    <div>
-      <h1>ToDo List</h1>
+    <div className="todo-list">
 
-      <form onSubmit={ handleSubmit }>
-        <input
-          type="text"
-          value={task}
-          placeholder="Add New Task"
-          onChange={ handleNewTask }
-        />
-        : 
-        <button
-          type="submit"
-          variant='contained'
-          color='primary'
-        >
-          Add Task
-        </button>  
-      </form>
-
-      <ul>
-        {todos.map((todo) => (
-          <li 
-            key={todo.id}
-            component='li'
-            style={ todo.isCompleted === true ? {textDecorationLine: 'line-through'}:{}}
+      <div className="todo-list__title">
+        <h1>ToDo List</h1>
+      </div>
+      
+      <div className="todo-list__form">
+        <form onSubmit={ handleSubmit }>
+          <input
+            type="text"
+            value={task}
+            placeholder="Add New Task"
+            onChange={ handleNewTask }
+          />
+          : 
+          <button
+            type="submit"
+            variant='contained'
+            color='primary'
           >
-            <input type="checkbox"
-                   onClick={ () => handleUpdateTask(todo.id) } />
-            {todo.task}
-            <button onClick={ () => handleRemoveTask(todo.id) }> x </button>
-          </li>
-        ))}
-      </ul>
+            Add Task
+          </button>  
+        </form>
+      </div>
+
+      <div className="todo-list__tasks">
+        <ul>
+          {todos.map((todo) => (
+            <li 
+              key={todo.id}
+              component='li'
+              style={ todo.isCompleted === true ? {textDecorationLine: 'line-through'}:{}}
+            >
+              {todo.task}
+              <button onClick={ () => handleRemoveTask(todo.id) }> x </button>
+              <button value="task" onClick={ () => handleUpdateTask(todo.id) }> ○ </button>
+
+              {/* <form>
+                <input
+                  type="text"
+                  name="issue"
+                  value={updatetask} 
+                  onChange={e => handleUpdate(e)}
+                />
+                <button
+                  type="submit"
+                  onClick={() => updateTask(todo.id)}
+                >
+                  更新
+                </button>
+              </form> */}
+            </li>
+          ))}
+        </ul>
+      </div>
+
     </div>
   );
 }
